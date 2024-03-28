ed4e40af2c332e057f763948cad9f1d4a2439cd2
Long Vu <longersoft@gmail.com>
2024-03-28 14:31:41 +0700

    fix checkov

--- infrastructure/0_prerequisites/main.tf
+++ infrastructure/0_prerequisites/main.tf
@@ -18,8 +18,10 @@ resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
 resource "aws_s3_bucket_public_access_block" "terraform_state_public_access_block" {
   bucket = aws_s3_bucket.terraform_state.id
 
-  block_public_acls   = true
-  block_public_policy = true
+  block_public_acls       = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+  ignore_public_acls      = true
 }
 
 resource "aws_dynamodb_table" "terraform_state_lock" {
